0b414b94a0e2fa2e34ad8558938773f41759eab6
Daniel Alexandre <dani.alex@live.se>
2021-11-12 11:39:44 +0100

    assignment 1

--- WebAppdemo/Controllers/HomeController.cs
+++ WebAppdemo/Controllers/HomeController.cs
@@ -12,5 +12,20 @@ namespace WebAppdemo.Controllers
         {
             return View();
         }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
     }
 }
--- WebAppdemo/Startup.cs
+++ WebAppdemo/Startup.cs
@@ -40,6 +40,8 @@ namespace WebAppdemo
                 );
 
             });
+
+
         }
     }
 }
