c3f5c420622211ccf9ee9811d6e18cdf3a74de1e
Daniel Alexandre <dani.alex@live.se>
2022-01-25 12:05:21 +0100

    1

---
+++ WebAppdemo/viewModel/LanguageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using WebAppdemo.Data;
+
+namespace WebAppdemo.viewModel
+{
+    public class LanguageViewModel
+    { 
+        public List<Language> LanguageView { get; set; }
+    }
+
+
+
+}
