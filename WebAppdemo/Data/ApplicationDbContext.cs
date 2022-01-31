ca3c0a7d0cdf2056adb8f87d9432d3231cecce87
Daniel Alexandre <dani.alex@live.se>
2022-01-31 10:47:57 +0100

    update

--- WebAppdemo/Data/ApplicationDbContext.cs
+++ WebAppdemo/Data/ApplicationDbContext.cs
@@ -13,8 +13,10 @@ namespace WebAppdemo.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            modelBuilder.Entity<PeopleModel>().HasData(new PeopleModel { Id = 1, Name = "Naruto", Skills = "Shadow Clones" });
-            modelBuilder.Entity<PeopleModel>().HasData(new PeopleModel { Id = 2, Name = "Sasuke", Skills = "Fireball" });
+            modelBuilder.Entity<PeopleModel>().HasData(new PeopleModel { Id = 1, Name = "Neo", PhoneNumber = "05199999", CityId = 1 });
+            modelBuilder.Entity<PeopleModel>().HasData(new PeopleModel { Id = 2, Name = "Mr.Smith", PhoneNumber = "08197777", CityId = 2 });
+            modelBuilder.Entity<PeopleModel>().HasData(new PeopleModel { Id = 3, Name = "Morpheus", PhoneNumber = "07198888", CityId = 3 });
+            modelBuilder.Entity<PeopleModel>().HasData(new PeopleModel { Id = 3, Name = "Trinity", PhoneNumber = "071234986", CityId = 4 });
         }
 
 
